9831f62496469b00f09676db341a42ee01ac4353
FITurnip <franklinirtr@gmail.com>
2024-02-26 17:37:36 +0700

    make to be static page

--- php/Main.php
+++ php/Main.php
@@ -6,37 +6,44 @@ require_once 'Product.php';
 $productList = [];
 $productId = 1;
 
-// Prompting the user to input at least three products
-echo "Enter details for at least three products:\n";
-for ($productId; $productId <= 3; $productId++) {
-    echo "Product $productId:\n";
-    echo "Enter product name: "; $name = readline();
-    echo "Enter brand: "; $brand = readline();
-    echo "Enter price: "; $price = (int)readline();
-
-    echo "Enter size: "; $size = readline();
-    echo "Enter material: "; $material = readline();
-    echo "Enter gender: "; $gender = readline();
-    echo "Enter color: "; $color = readline();
-    echo "Enter sleeve type: "; $sleeveType = readline();
-
-    // Adding the input product to the array
-    $productList[] = new Product($size, $material, $gender, $color, $sleeveType, $productId, $name, $brand, $price);
-
-    echo "\n";
-}
-
-// Printing the list of products in a table
-echo "List of Products:\n";
-$totalColumnLen = 100;
-echo str_repeat("-", $totalColumnLen) . "\n";
-echo "| " . str_pad("Product ID", 10) . " | " . str_pad("Name", 15) . " | " . str_pad("Brand", 15) . " | " . str_pad("Price", 10) . " | " . str_pad("Size", 10) . " | " . str_pad("Material", 15) . " | " . str_pad("Gender", 10) . " | " . str_pad("Color", 10) . " | " . str_pad("Sleeve Type", 15) . " |\n";
-echo str_repeat("-", $totalColumnLen) . "\n";
-
-foreach ($productList as $product) {
-    echo "| " . str_pad($product->getIdProduct(), 10) . " | " . str_pad($product->getName(), 15) . " | " . str_pad($product->getBrand(), 15) . " | " . str_pad($product->getPrice(), 10) . " | " . str_pad($product->getSize(), 10) . " | " . str_pad($product->getMaterial(), 15) . " | " . str_pad($product->getGender(), 10) . " | " . str_pad($product->getColor(), 10) . " | " . str_pad($product->getSleeveType(), 15) . " |\n";
-}
-
-echo str_repeat("-", $totalColumnLen) . "\n";
-
+$productList[] = new Product("Size 1", "Material 1", "Man", "Yellow", "Sleeve Type 1", 1, "Name 1", "Brand 1", 1000000);
+$productList[] = new Product("Size 2", "Material 2", "Man", "Yellow", "Sleeve Type 2", 2, "Name 2", "Brand 2", 2000000);
+$productList[] = new Product("Size 4", "Material 4", "Man", "Yellow", "Sleeve Type 4", 4, "Name 4", "Brand 4", 4000000);
+$productList[] = new Product("Size 8", "Material 8", "Man", "Yellow", "Sleeve Type 8", 8, "Name 8", "Brand 8", 8000000);
+$productList[] = new Product("Size 16", "Material 16", "Man", "Yellow", "Sleeve Type 16", 16, "Name 16", "Brand 16", 16000000);
+$productList[] = new Product("Size 32", "Material 32", "Man", "Yellow", "Sleeve Type 32", 32, "Name 32", "Brand 32", 32000000);
+$productList[] = new Product("Size 64", "Material 64", "Man", "Yellow", "Sleeve Type 64", 64, "Name 64", "Brand 64", 64000000);
+$productList[] = new Product("Size 128", "Material 128", "Man", "Yellow", "Sleeve Type 128", 128, "Name 128", "Brand 128", 128000000);
+$productList[] = new Product("Size 256", "Material 256", "Man", "Yellow", "Sleeve Type 256", 256, "Name 256", "Brand 256", 256000000);
+$productList[] = new Product("Size 1024", "Material 1024", "Man", "Yellow", "Sleeve Type 1024", 1024, "Name 1024", "Brand 1024", 1024000000);
+$productList[] = new Product("Size 2048", "Material 2048", "Man", "Yellow", "Sleeve Type 2048", 2048, "Name 2048", "Brand 2048", 2048000000);
 ?>
+
+<table border="1">
+    <tr>
+        <th>Product ID</th>
+        <th>Name</th>
+        <th>Brand</th>
+        <th>Price</th>
+        <th>Size</th>
+        <th>Material</th>
+        <th>Gender</th>
+        <th>Color</th>
+        <th>Sleeve Type</th>
+    </tr>
+    <?php
+    foreach($productList as $product) {
+        echo "<tr>
+            <td>{$product->getIdProduct()}</td>
+            <td>{$product->getName()}</td>
+            <td>{$product->getBrand()}</td>
+            <td>{$product->getPrice()}</td>
+            <td>{$product->getSize()}</td>
+            <td>{$product->getMaterial()}</td>
+            <td>{$product->getGender()}</td>
+            <td>{$product->getColor()}</td>
+            <td>{$product->getSleeveType()}</td>
+        </tr>";
+    }
+    ?>
+</table>
